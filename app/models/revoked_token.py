fca708cf06148b0f8ace7ce4cd26fdf24327ccd6
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2025-09-20 21:57:16 -0300

    Se crea metodo para guardar un token en una base de datos

--- app/models/revoked_token.py
+++ app/models/revoked_token.py
@@ -12,5 +12,11 @@ class RevokedToken(db.Model):
     jti = db.Column(db.String(120), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
+    @classmethod
+    def add(cls, jti):
+        revoked_token = cls(jti=jti)
+        db.session.add(revoked_token)
+        db.session.commit()
+        
     def __init__(self, jti):
         self.jti = jti
